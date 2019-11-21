55ce5edf145509044a9214daa70f9e9a83610218
Steven Roland <steven@mojave.io>
2019-11-20 20:07:04 -0500

    Add front end scaffolding, add basic threads test and views

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function path()
+    {
+        return '/threads/' . $this->id;
+    }
 }
