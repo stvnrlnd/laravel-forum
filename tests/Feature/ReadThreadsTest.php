a4fc650ba114361be343c177286f75a4eecfb50c
Steven Roland <steven@mojave.io>
2019-11-20 20:36:44 -0500

    Add tests for thread replies, add replies to single thread view

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_single_thread()
+    {
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_to_a_thread()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
+}
