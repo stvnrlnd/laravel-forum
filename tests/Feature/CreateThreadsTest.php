8d345b0c8015b5869288fd84213adf4f350c88bd
Steven Roland <steven@mojave.io>
2019-11-23 13:49:35 -0500

    Add ability for users to create threads

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function guests_may_not_create_threads()
+    {
+        $this->withoutExceptionHandling();
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->post('/threads', []);
+    }
+
+
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->actingAs(factory('App\User')->create());
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
