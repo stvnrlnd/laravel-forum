4384d15a3f6e0d5e1f627974dc2ffb1ab6173248
Steven Roland <steven@mojave.io>
2019-11-25 20:31:44 -0500

    Add channel tests

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 // Route::resource('/threads', 'ThreadController');
 Route::get('/threads', 'ThreadController@index');
 Route::get('/threads/create', 'ThreadController@create');
+Route::get('/threads/{channel}', 'ThreadController@index');
 Route::post('/threads', 'ThreadController@store');
 Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
 Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
