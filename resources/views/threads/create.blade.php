9ac7c8d90a7ab25f98ec5ee631f95319b082c5c6
Steven Roland <steven@mojave.io>
2019-11-26 19:31:13 -0500

    Add link to thread creation form and required channel input

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,15 +13,37 @@
                         @csrf
                         @method('POST')
 
+                        <div class="form-grou">
+                            <label for="selectChannel">Choose a Channel</label>
+                            <select name="channel_id" id="selectChannel" class="form-control" required>
+                                <option value="">Choose One...</option>
+                                @foreach (App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}"{{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
                         <div class="form-group">
                             <label for="textTitle">Title:</label>
-                            <input type="text" name="title" id="textTitle" class="form-control">
+                            <input type="text" name="title" id="textTitle" class="form-control" value="{{ old('title') }}" required>
                         </div>
+
                         <div class="form-group">
                             <label for="textareaBody">Body:</label>
-                            <textarea name="body" id="textareaBody" cols="30" rows="10" class="form-control"></textarea>
+                            <textarea name="body" id="textareaBody" cols="30" rows="10" class="form-control" required>{{ old('body') }}</textarea>
                         </div>
-                        <button type="submit" class="btn btn-secondary">Publish</button>
+
+                        <div class="form-grou">
+                            <button type="submit" class="btn btn-secondary">Publish</button>
+                        </div>
+
+                        @if (count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
                     </form>
                 </div>
             </div>
