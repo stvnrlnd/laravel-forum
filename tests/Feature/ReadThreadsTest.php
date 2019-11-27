42c5632d321295ea394f43703c9e67ee1bc36dfe
Steven Roland <steven@mojave.io>
2019-11-27 03:39:24 +0000

    Apply fixes from StyleCI

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -63,11 +63,11 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_filter_threads_by_any_username()
     {
         $this->signIn(create('App\User', [
-            'name' => 'John'
+            'name' => 'John',
         ]));
 
         $threadByJohn = create('App\Thread', [
-            'user_id' => auth()->id()
+            'user_id' => auth()->id(),
         ]);
         $threadNotByJohn = create('App\Thread');
 
