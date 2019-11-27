556880df32c314f9cfb51c4808e513130b4d9776
Steven Roland <steven@mojave.io>
2019-11-26 22:39:05 -0500

    Add ability for users to filter threads by author

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -58,4 +58,21 @@ class ReadThreadsTest extends TestCase
             ->assertSee($threadInChannel->title)
             ->assertDontSee($threadNotInChannel->title);
     }
+
+    /** @test */
+    public function a_user_can_filter_threads_by_any_username()
+    {
+        $this->signIn(create('App\User', [
+            'name' => 'John'
+        ]));
+
+        $threadByJohn = create('App\Thread', [
+            'user_id' => auth()->id()
+        ]);
+        $threadNotByJohn = create('App\Thread');
+
+        $this->get('threads?by=John')
+            ->assertSee($threadByJohn->title)
+            ->assertDontSee($threadNotByJohn->title);
+    }
 }
