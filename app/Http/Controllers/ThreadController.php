12f5b9e5b76694f9ed579820de73fb44aa2c52bd
Steven Roland <steven@mojave.io>
2019-11-28 12:27:26 -0500

    Refactor thread show view, add pagination

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -75,7 +75,10 @@ class ThreadController extends Controller
      */
     public function show(Channel $channel, Thread $thread)
     {
-        return view('threads.show', compact('thread'));
+        return view('threads.show', [
+            'thread' => $thread,
+            'replies' => $thread->replies()->paginate(25)
+        ]);
     }
 
     /**
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,12 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card mb-3">
                 <div class="card-header">
-                <a href="#">{{ $thread->creator->name }}</a> posted:
-                    {{ $thread->title }}
+                    <a href="#">{{ $thread->creator->name }}</a> posted: {{ $thread->title }}
                 </div>
 
                 <div class="card-body">
@@ -19,34 +18,37 @@
                     {{ $thread->body }}
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @foreach ($thread->replies as $reply)
+            @foreach ($replies as $reply)
                 @include('threads.reply')
             @endforeach
+
+            {{ $replies->links() }}
+
+            @auth
+                <form action="{{ $thread->path() . '/replies' }}" method="POST">
+
+                    @csrf
+                    @method('POST')
+
+                    <div class="form-group">
+                        <textarea name="body" id="textareaBody" class="form-control" cols="30" rows="10" placeholder="Have something to say?"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-secondary">Post</button>
+                </form>
+            @else
+                <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+            @endauth
         </div>
-    </div>
-
-    @auth
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <form action="{{ $thread->path() . '/replies' }}" method="POST">
-
-                @csrf
-                @method('POST')
-
-                <div class="form-group">
-                    <textarea name="body" id="textareaBody" class="form-control" cols="30" rows="10" placeholder="Have something to say?"></textarea>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p>This thread was published {{ $thread->created_at->diffForHumans() }}
+                    by <a href="#">{{ $thread->creator->name }}</a>,
+                    and currently has {{ $thread->replies_count }} {{ Str::plural('comment', $thread->replies_count) }}.</p>
                 </div>
-                <button type="submit" class="btn btn-secondary">Post</button>
-            </form>
+            </div>
         </div>
     </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-    @endauth
 </div>
 @endsection
