35bd898e55b1fef0ccc5341d08755f860fd14ff6
Steven Roland <steven@mojave.io>
2019-12-08 20:00:29 -0500

    Add favorite button

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -41,7 +41,9 @@ class FavoriteController extends Controller
      */
     public function store(Reply $reply)
     {
-        return $reply->favorite();
+        $reply->favorite();
+
+        return back();
     }
 
     /**
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,6 +1,20 @@
 <div class="card mb-2">
     <div class="card-header">
-        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}
+        <div class="d-flex justify-content-between">
+            <h5>
+                <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}
+            </h5>
+            <div>
+                <form action="/replies/{{ $reply->id }}/favorites" method="POST">
+                    @csrf
+                    @method('POST')
+
+                    <button type="submit" class="btn btn-light"{{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ Str::plural('favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="card-body">
         {{ $reply->body }}
