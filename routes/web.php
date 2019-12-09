ba49ec8cd3d8c151d6e4c28128685f45c27890ef
Steven Roland <steven@mojave.io>
2019-12-08 19:21:14 -0500

    Add ability to to favorite a reply

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::resource('/threads', 'ThreadController');
 Route::get('/threads', 'ThreadController@index');
 Route::get('/threads/create', 'ThreadController@create');
 Route::get('/threads/{channel}', 'ThreadController@index');
 Route::post('/threads', 'ThreadController@store');
 Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
 Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
+Route::post('/replies/{reply}/favorites', 'FavoriteController@store');
 
 Auth::routes();
 
